eb5b6dfbe01fea2acae67167447b32719de769db
IsqDantas <isaque.dantas@escolar.ifrn.edu.br>
2023-11-22 11:04:46 -0300

    nao-sei

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+
+class MinhaApp:
+    def __init__(self):
+        self.root = Tk()
+        self.root.mainloop()
+
+
+def main():
+    MinhaApp()
+
+
+if __name__ == '__main__':
+    main()
